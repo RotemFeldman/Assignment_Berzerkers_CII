07b762417a2010869225d3024c2ef905b9a9de76
RotemFeldman <128305863+RotemFeldman@users.noreply.github.com>
2024-01-22 15:03:59 +0200

    fight working

--- Program.cs
+++ Program.cs
@@ -8,20 +8,107 @@ namespace C_II_1stAssignment
     {
         static void Main(string[] args)
         {
-            Dice die = new Dice(1,8,9);
-            Dice die2 = new Dice(1,8,8);
+            List<Unit> BlueTeam = new List<Unit>();
+            HumanCowboy cowboy = new HumanCowboy();
+            BlueTeam.Add(cowboy);
+            HumanKnight knight = new HumanKnight();
+            BlueTeam.Add(knight);
+            HumanMonk monk = new HumanMonk();
+            BlueTeam.Add(monk);
 
-            int r = die.Roll();
 
-            Console.WriteLine(r);
-            Console.WriteLine(die.ToString());
-            Console.WriteLine(die.GetHashCode());
-            Console.WriteLine(die2.GetHashCode());
+            List<Unit> RedTeam = new List<Unit>();
+            DragonbornBrute brute = new DragonbornBrute();
+            RedTeam.Add(brute);
+            DragonbornMage mage = new DragonbornMage();
+            RedTeam.Add(mage);
+            DragonbornPaladin paladin = new DragonbornPaladin();
+            RedTeam.Add(paladin);
 
-            if(die.Equals(die2))
+
+            int BlueCount = 0;
+            int RedCount = 0;
+
+            bool _bothTeamsAlive = true;
+            
+
+            while(_bothTeamsAlive)
             {
-                Console.WriteLine("eq");
+                BlueAttack(BlueCount);
+
+                if (!ContinueFight(BlueTeam, RedTeam))
+                    break;
+                Console.WriteLine();
+
+                RedAttack(RedCount);
+
+                if (!ContinueFight(BlueTeam, RedTeam))
+                    break;
+                Console.WriteLine();
             }
+
+            if(BlueTeam.Count > 0)
+            {
+                Console.WriteLine("Blue Wins");
+            }
+            else
+            {
+                Console.WriteLine("Red Wins");
+            }
+
+
+
+
+            void BlueAttack(int i)
+            {
+                int def = Random.Shared.Next(0, RedTeam.Count);
+
+                BlueTeam[i].Attack(RedTeam[def]);
+
+                if (RedTeam[def].IsDead)
+                {
+                    RedTeam.Remove(RedTeam[def]);
+                    if (RedCount > RedTeam.Count - 1)
+                        RedCount = 0;
+                }
+
+                BlueCount++;
+                if(BlueCount > BlueTeam.Count -1)
+                    BlueCount = 0;
+            }
+
+            void RedAttack(int i)
+            {
+                int def = Random.Shared.Next(0, BlueTeam.Count);
+
+                RedTeam[i].Attack(BlueTeam[def]);
+
+                if (BlueTeam[def].IsDead)
+                {
+                    BlueTeam.Remove(BlueTeam[def]);
+                    if (BlueCount > BlueTeam.Count - 1)
+                        BlueCount = 0;
+                }
+
+                RedCount++;
+                if (RedCount > RedTeam.Count -1)
+                    RedCount = 0;
+            }
+
+            bool ContinueFight(List<Unit> blue, List<Unit> red)
+            {
+                if (blue.Count == 0 || red.Count == 0)
+                {
+                    _bothTeamsAlive = false;
+                    return false;
+                }
+                else
+                    return true;
+            }
+
+
         }
+
+        
     }
 }
